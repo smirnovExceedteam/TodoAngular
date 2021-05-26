1988b1b9d974b7ea4280b97bcd58a1493fc5deeb
sergey <smirnov.exceedteam@gmail.com>
2021-05-26 17:36:27 +0300

    kek

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
+
 export class AppComponent {
-  title = 'todosNew';
+  text: string = ""
+  input = false;
+
+  onKeyDown(event: any) {
+    if (event.keyCode === 13) {
+      this.text = String(event.target.value).trim()
+      event.target.value = ""
+    }
+  }
+
+  constructor() {
+    setTimeout(() => {
+      this.input = true
+    }, 10000)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './components/input/input.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BorderOfTasksComponent } from './components/border-of-tasks/border-of-tasks.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    FooterComponent,
+    BorderOfTasksComponent,
+    LogoComponent,
+    TaskItemComponent,
   ],
   imports: [
     BrowserModule
